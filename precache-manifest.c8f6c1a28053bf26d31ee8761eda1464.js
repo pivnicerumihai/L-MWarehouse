9cce9b4e91f94de6c99c350f7b2921afc4eadd54
ML Pivniceru <mcpivniceru@gmail.com>
2020-08-27 13:01:11 +0100

    Updates

---
+++ precache-manifest.c8f6c1a28053bf26d31ee8761eda1464.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5394a6ca1cda3e6e9537ad9729383bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4cacb2d09e9b2bc864a",
+    "url": "/static/css/main.fbc21fa5.chunk.css"
+  },
+  {
+    "revision": "3392895c290518b7bf50",
+    "url": "/static/js/2.65c0edb3.chunk.js"
+  },
+  {
+    "revision": "edf315e69a5f5d07e2e2ba8c1f814fbe",
+    "url": "/static/js/2.65c0edb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4cacb2d09e9b2bc864a",
+    "url": "/static/js/main.cbf67441.chunk.js"
+  },
+  {
+    "revision": "f3c02b4fbcab6ad39ff4",
+    "url": "/static/js/runtime-main.987eaa6d.js"
+  },
+  {
+    "revision": "562303504d048768271efbf324cb750c",
+    "url": "/static/media/logo_file.56230350.png"
+  },
+  {
+    "revision": "28eb6f199c142a46fdebe5758c48d5e1",
+    "url": "/static/media/slider-1.28eb6f19.jpg"
+  },
+  {
+    "revision": "90da51ebef5570727f667f56825246a9",
+    "url": "/static/media/slider-2.90da51eb.jpg"
+  },
+  {
+    "revision": "7ce4c0128a451a40f77016b3dc7150db",
+    "url": "/static/media/slider-3.7ce4c012.jpg"
+  }
+]);
